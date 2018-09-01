b758613fc14650dc3d699b2bf3f6d821e1781a97
Zoryna <jsoryna@gmail.com>
2018-08-31 23:58:59 -0500

    Tested making points appear on the board through user input

--- board.java
+++ board.java
@@ -2,22 +2,24 @@
 The board for tic tac toe
 
 Coded by Janeen Soria
-Last modified August 31, 2018
+Last modified September 1, 2018
 */
 
-import javafx.scene.canvas.GraphicsContext;
 import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.ImageView;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.shape.ArcType;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
 import javafx.scene.Scene;
 import javafx.scene.Group;
-import javafx.stage.Stage;
 import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+
+import java.util.Scanner;
 
 public class board extends Application
 {
@@ -47,13 +49,27 @@ public class board extends Application
     root.getChildren().add(box);
     root.getChildren().add(canvas);
 
-    stage.setTitle("Please Don't Die Today");
+    stage.setTitle("Tic Tac Toe");
     stage.setWidth(415);
     stage.setHeight(200);
     stage.setScene(scene);
     stage.sizeToScene();
     stage.show();
 
+    //test making points
+    Scanner keyboard = new Scanner (System.in);
+
+    System.out.println("Enter a number");
+    int input = keyboard.nextInt();
+      if (input == 1)
+      {
+        Circle node0 = new Circle(); //would have 9 shapes maximum
+        node0.setCenterX(453);
+        node0.setCenterY(178);
+        node0.setRadius(5);
+        root.getChildren().add(node0);
+      }
+    System.out.println("After first point");
 
   }
 
