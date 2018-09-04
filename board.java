659f16d23a5b8f9d128220d950033b500a58187b
johare1 <johare@luc.edu>
2018-09-04 17:24:43 -0500

    added initialization of players and sections, created list of sections to store them all in

--- board.java
+++ board.java
@@ -18,6 +18,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Circle;
 import javafx.scene.paint.Color;
+import java.util.*;
 
 //import java.util.Scanner;
 
@@ -67,11 +68,42 @@ public class board extends Application
     root.getChildren().add(node0);
     */
 
+    /*TODO set user input to these strings*/
+    String p1Name = "";
+    String p2Name = "";
 
     gamePlay play = new gamePlay();
     play.startDisplay();
 
 
+    /*initialize players and sections of board*/
+    Player p1 = new Player(false ,"X", p1Name);
+    Player p2 = new Player(false ,"O", p2Name);
+
+    Section s0 = new Section(0,"",false);
+    Section s1 = new Section(1,"",false);
+    Section s2 = new Section(2,"",false);
+    Section s3 = new Section(3,"",false);
+    Section s4 = new Section(4,"",false);
+    Section s5 = new Section(5,"",false);
+    Section s6 = new Section(6,"",false);
+    Section s7 = new Section(7,"",false);
+    Section s8 = new Section(8,"",false);
+
+    List<Section> sectionList = new ArrayList<Section>();
+
+    sectionList.add(s0);
+    sectionList.add(s1);
+    sectionList.add(s2);
+    sectionList.add(s3);
+    sectionList.add(s4);
+    sectionList.add(s5);
+    sectionList.add(s6);
+    sectionList.add(s7);
+    sectionList.add(s8);
+
+
+    
 
 
   }
